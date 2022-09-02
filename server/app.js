3f0b219861b1ca00258643163f5379d1ecab949b
Dilyan Penev <dilyan.penev@student.manchester.ac.uk>
2022-09-02 10:34:44 +0300

    Create a request for the city of the day

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const seedrandom = require('seedrandom');
 const config = require('./config');
 const CityModel = require('./models/Cities');
 
@@ -12,8 +13,13 @@ mongoose.connect(config.mongoDBConnectionString);
 
 const port = process.env.PORT || config.serverPort;
 
-app.get('/city-api/getCities', (_req, res) => {
-    CityModel.find({}, (err, result) => {
+app.get('/city-api/getTodaysCity', (_req, res) => {
+    // seed the random number generator with today's date
+    const rng = seedrandom(new Date().toISOString().slice(0, 10));
+    const randomNumber = rng();
+    const todayIndex = Math.floor(randomNumber * 257)
+    // TODO add .skip(todayIndex) when DB is populated
+    CityModel.findOne().select('name').exec((err, result) => {
         if (err) {
             res.json(err);
         }
